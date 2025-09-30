1d547e851224a676e9780dd9a71c087b2be977ff
parinlexan <parinlexan@yandex.ru>
2025-10-01 01:12:01 +0300

    добавила проверки и файл с описанием

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # qa_python
+
+test_add_new_book_add_one_book_adds_book - добавление одной книги
+
+test_add_new_book_add_two_books_adds_book - добавление двух книг
+
+def test_add_new_book_add_symbol_length_check_adds_book - проверка граничных валидных (1, 39, 40) значений длины книги
+
+test_add_new_book_add_same_book_doesnt_add_book - добавление книги с одним и тем же названием
+
+test_add_new_book_41_and_0_symbol_book_doesnt_add_book - проверка граничных не валидных (0, 41) значений длины книги
+
+test_set_book_genre_from_book_genre_adds_genre - утсановка жанра для книги
+
+test_set_book_genre_random_string_doesnt_add_genre - отсутствие установки жанра для книги
+
+test_get_book_genre_existing_book_gives_info - получение жанра существуюей книги
+
+test_get_book_genre_random_string_doesnt_give_info - отсутствие информации при попытки получить жанр несуществующей книги
+
+test_get_books_for_children_child_appropriate_genres_shows_genres - получение списка книг с разрешенным для детей жанром
+
+test_get_books_for_children_child_inappropriate_genres_doesnt_shows_genres - отсутсвие вывода запрещнных для детей жанров
+
+test_get_books_with_specific_genre_existed_genre_gives_books - получение существующей книги с определнным жанром
+
+test_get_list_of_favorites_books_gives_info - получение списка любимых книг
+
+test_add_book_in_favorites_adds_book - добавление книги в список любимых
+
+test_delete_book_from_favorites_removes_the_book - удаление книги из списка любимых
--- tests.py
+++ tests.py
@@ -1,24 +1,104 @@
+import random
+
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    def test_add_new_book_add_one_book_adds_book(self, collector):
+        collector.add_new_book('Ключворды')
 
-        # добавляем две книги
+        assert len(collector.get_books_genre()) == 1 and 'Ключворды' in collector.get_books_genre()
+
+    def test_add_new_book_add_two_books_adds_book(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize('name', ['Я', 'Чипи-чипи чапа-чапа дуби-дуби даба-даба', 'Чипи-чипи чапа-чапа дуби-дуби даба-даба!'])
+    def test_add_new_book_add_symbol_length_check_adds_book(self, collector, name):
+        collector.add_new_book(name)
+
+        assert len(collector.get_books_genre()) == 1 and name in collector.get_books_genre()
+
+    def test_add_new_book_add_same_book_doesnt_add_book(self, collector):
+        collector.add_new_book('Кот в сандалях')
+        collector.add_new_book('Кот в сандалях')
+
+        assert len(collector.get_books_genre()) == 1 and 'Кот в сандалях' in collector.get_books_genre()
+
+    @pytest.mark.parametrize('name', ['Чипи-чипи чапа-чапа дуби-дуби даба-даба!!', ''])
+    def test_add_new_book_41_and_0_symbol_book_doesnt_add_book(self, collector, name):
+        collector.add_new_book(name)
+
+        assert len(collector.get_books_genre()) == 0
+
+
+
+    def test_set_book_genre_from_book_genre_adds_genre(self, pre_added_book):
+        genre = random.choice(BooksCollector().genre)
+        pre_added_book.set_book_genre('История чилибони', genre)
+
+        assert genre in list(pre_added_book.get_books_genre().values())
+
+    def test_set_book_genre_random_string_doesnt_add_genre(self, pre_added_book):
+        pre_added_book.set_book_genre('История чилибони', 'genre')
+
+        assert list(pre_added_book.get_books_genre().values()) == ['']
+
+
+    def test_get_book_genre_existing_book_gives_info(self, pre_added_book):
+        genre = random.choice(BooksCollector().genre)
+        pre_added_book.set_book_genre('История чилибони', genre)
+
+        assert pre_added_book.get_book_genre('История чилибони') == genre
+
+    def test_get_book_genre_random_string_doesnt_give_info(self, pre_added_book):
+        genre = random.choice(BooksCollector().genre)
+        pre_added_book.set_book_genre('История чилибони', genre)
+
+        assert pre_added_book.get_book_genre('rrrrr') == None
+
+
+
+    @pytest.mark.parametrize('genre', ['Фантастика', 'Мультфильмы', 'Комедии'])
+    def test_get_books_for_children_child_appropriate_genres_shows_genres(self, pre_added_book, genre):
+        pre_added_book.set_book_genre('История чилибони', genre)
+
+        assert pre_added_book.get_books_for_children() == list(pre_added_book.get_books_genre().keys())
+
+    @pytest.mark.parametrize('genre', ['Ужасы', 'Детективы'])
+    def test_get_books_for_children_child_inappropriate_genres_doesnt_shows_genres(self, pre_added_book, genre):
+        pre_added_book.set_book_genre('История чилибони', genre)
+
+        assert pre_added_book.get_books_for_children() == []
+
+
+
+    @pytest.mark.parametrize('genre', ['Фантастика', 'Мультфильмы', 'Комедии', 'Ужасы', 'Детективы'])
+    def test_get_books_with_specific_genre_existed_genre_gives_books(self, collector, genre):
+        collector.add_new_book('Сумерки')
+        collector.set_book_genre('Сумерки', random.choice(BooksCollector().genre))
+        collector.add_new_book('Рассвет')
+        collector.set_book_genre('Рассвет', genre)
+
+        assert 'Сумерки' in collector.get_books_with_specific_genre(genre) or 'Рассвет' in collector.get_books_with_specific_genre(genre)
+
+
+    def test_get_list_of_favorites_books_gives_info(self, pre_added_book):
+        pre_added_book.add_book_in_favorites('История чилибони')
+
+        assert pre_added_book.get_list_of_favorites_books() != []
+
+    def test_add_book_in_favorites_adds_book(self, pre_added_book):
+        pre_added_book.add_book_in_favorites('История чилибони')
+
+        assert pre_added_book.get_list_of_favorites_books() != []
+
+    def test_delete_book_from_favorites_removes_the_book(self, pre_added_book):
+        pre_added_book.add_book_in_favorites('История чилибони')
+        pre_added_book.delete_book_from_favorites('История чилибони')
+
+        assert pre_added_book.get_list_of_favorites_books() == []
